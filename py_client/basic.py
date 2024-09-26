6beada5a933a75616f98862eed818c60ada549ed
AleksandrKosmylev <alex.kosmylev@gmail.com>
2024-09-26 23:10:46 +0300

    add test enpoints upt

--- py_client/basic.py
+++ py_client/basic.py
@@ -4,5 +4,11 @@ import requests
 endpoint = "https://httpbin.org/status/200"
 endpoint = "https://httpbin.org/anything"
 
-get_response = requests.get(endpoint)  #HTTP Request
-print(get_response.text)
+#get_response = requests.get(endpoint)  #HTTP Request
+get_response = requests.get(endpoint, json = {"query" : "Hello world"})
+# get_response = requests.get(endpoint, data = {"query" : "Hello world"})
+
+### python py_client/basic.py
+
+# print(get_response.text)
+print(get_response.json())
