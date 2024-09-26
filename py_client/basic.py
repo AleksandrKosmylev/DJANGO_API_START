efc64cb6aa6d0ae55b07691b480ed9b85d86cb66
AleksandrKosmylev <alex.kosmylev@gmail.com>
2024-09-26 22:57:13 +0300

    add test enpoints

---
+++ py_client/basic.py
@@ -0,0 +1,8 @@
+from operator import ge
+import requests
+
+endpoint = "https://httpbin.org/status/200"
+endpoint = "https://httpbin.org/anything"
+
+get_response = requests.get(endpoint)  #HTTP Request
+print(get_response.text)
