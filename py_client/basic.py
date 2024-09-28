9e9456fdf68bde4869859e5c803f883ed9417330
AleksandrKosmylev <alex.kosmylev@gmail.com>
2024-09-28 17:20:05 +0300

    upt check endpoints

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,7 +2,7 @@ from operator import ge
 import requests
 
 endpoint = "https://httpbin.org/status/200"
-endpoint = "https://httpbin.org/anything"
+#endpoint = "https://httpbin.org/anything"
 
 #get_response = requests.get(endpoint)  #HTTP Request
 get_response = requests.get(endpoint, json = {"query" : "Hello world"})
@@ -11,4 +11,5 @@ get_response = requests.get(endpoint, json = {"query" : "Hello world"})
 ### python py_client/basic.py
 
 # print(get_response.text)
-print(get_response.json())
+#print(get_response.json())
+print(get_response.status_code)
