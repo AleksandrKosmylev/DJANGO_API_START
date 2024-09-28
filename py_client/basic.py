371454cc23d086a7c69e806853dc8b29cf268645
AleksandrKosmylev <alex.kosmylev@gmail.com>
2024-09-28 18:43:50 +0300

    start api

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,15 +1,14 @@
-from operator import ge
+# from operator import get
 import requests
 
-endpoint = "https://httpbin.org/status/200"
-#endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/status/200"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/api/"
 
-#get_response = requests.get(endpoint)  #HTTP Request
 get_response = requests.get(endpoint, json = {"query" : "Hello world"})
 # get_response = requests.get(endpoint, data = {"query" : "Hello world"})
 
-### python py_client/basic.py
 
-# print(get_response.text)
-#print(get_response.json())
+print(get_response.text)
+print(get_response.json()['message'])
 print(get_response.status_code)
