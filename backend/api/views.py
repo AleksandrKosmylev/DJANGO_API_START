29f071de6bab2f8cbca1ccf323af94dad22d089b
AleksandrKosmylev <alex.kosmylev@gmail.com>
2024-09-29 21:24:49 +0300

    echoing response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,19 @@
+from dataclasses import dataclass
 from django.http import JsonResponse
+import json
 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "This is api response"})
+    print(request.GET) # url query params
+    body = request.body # byte sting of data
+    data = {}
+    try:
+        data = json.loads(body) # string of json data -> Python dict
+    except:
+        pass
+    print(data)
+    print(request.headers)
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
