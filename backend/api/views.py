f6949db2b944cfe8f1438d14cd5c1769c5cefe12
AleksandrKosmylev <alex.kosmylev@gmail.com>
2024-10-20 21:55:44 +0300

    add serializer

--- backend/api/views.py
+++ backend/api/views.py
@@ -10,14 +10,20 @@ import json
 from django.forms.models import model_to_dict
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from products.serializers import ProductSerializer
 
 @api_view(["GET","POST"])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by("?").first()
     data = {}
+    """
+    model_data = Product.objects.all().order_by("?").first()
     if model_data:
-        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+        data = model_to_dict(model_data, fields=['id', 'title', 'price','sale_price'])
+    """
+    instance = Product.objects.all().order_by("?").first()
+    if instance: 
+        # data = model_to_dict(instance, fields=['id', 'title', 'price','sale_price'])
+        data = ProductSerializer(instance).data
     return Response(data)
 
 
